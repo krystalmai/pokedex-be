c88bd517a83f5432a53a28840efabe25e35e108b
Krystal <krystalmai@gmail.com>
2022-09-23 12:13:54 +0700

    reshape db

--- createPokemon.js
+++ createPokemon.js
@@ -4,19 +4,19 @@ const csv = require("csvtojson");
 const createPokemon = async () => {
   
   let newData = await csv().fromFile("pokemon.csv"); //convert csv to json array
- 
 
   newData = newData
     .map((pokemon, index) => { // reshape data objects 
+
       return {
         id: index + 1,
         name: pokemon.Name,
-        types: [pokemon.Type1, pokemon.Type2],
+        types: pokemon.Type2 ? [pokemon.Type1, pokemon.Type2 ] : [pokemon.Type1],
         url: `http://localhost:8000/images/${index + 1}.png`,
       };
     })
     .filter((pokemon) => pokemon.id <= 721);
-  
+
   // retrieve db.json content
   let data = JSON.parse(fs.readFileSync("db.json"));
 
